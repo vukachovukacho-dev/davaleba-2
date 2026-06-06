0c43f1083b7247238a8a59404bd685f2545412bb
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-06 14:22:39 +0400

    Add project files.

---
+++ davaleba-2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace davaleba_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("enter your age:");
+            //string ageStr = Console.ReadLine();
+
+            //bool isValidAge = int.TryParse(ageStr, out int age);
+
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("you can vote");
+            //}
+            //else if (isValidAge && age <18)
+            //{
+            //    Console.WriteLine("you cant vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid input");
+            //}
+
+
+
+
+            //Console.WriteLine("first number");
+            //string num1Str = Console.ReadLine();
+
+            //Console.WriteLine("second number");
+            //string num2Str = Console.ReadLine();
+
+            //Console.WriteLine("thrid number");
+            //string num3Str = Console.ReadLine();
+
+            //bool isValidNum1 = int.TryParse(num1Str, out int num1);
+            //bool isValidNum2 = int.TryParse(num2Str, out int num2);
+            //bool isValidNum3 = int.TryParse(num3Str, out int num3);
+
+            //if (num1 >= num2 && num1 >= num3)
+            //{
+            //    Console.WriteLine("biggest: " + num1);
+            //}
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+            //    Console.WriteLine("biggest: " + num2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("biggest: " + num3);
+            //}
+
+
+
+        }
+    }
+}
