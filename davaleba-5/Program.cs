2207484d153806014bcf648dfa5d37e92d7c623b
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-09 23:47:25 +0400

    davaleba-5

---
+++ davaleba-5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace davaleba_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 7, 3 };          
+            int[] array2 = { 4, 5, 6 };
+        
+            int[] resultArray = new int[array1.Length + array2.Length];
+           
+            for (int i = 0; i < array1.Length; i++)
+            {
+                resultArray[i] = array1[i];
+            }
+
+            
+            for (int i = 0; i < array2.Length; i++)
+            {
+                resultArray[array1.Length + i] = array2[i];
+            }
+
+            
+            Console.Write("result = [");
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i]);
+
+                if (i < resultArray.Length - 1)
+                    Console.Write(", ");
+            }
+                Console.WriteLine("]");
+        }
+    }
+}
