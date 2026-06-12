d819fc57993d7e0f1455467529328e367313423c
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-12 20:49:44 +0400

    davaleba-6

---
+++ Daavaleba-6/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Daavaleba_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int[][] scores =
+            //{
+            //    new int[] { 90, 85, 88 },      
+            //    new int[] { 75, 80 },          
+            //    new int[] { 100, 95, 98, 92 }  
+            //};
+
+            //    for (int i = 0; i < scores.Length; i++)
+            //    {
+            //        double sum = 0;
+
+            //        for (int j = 0; j < scores[i].Length; j++)
+            //        {
+            //            sum += scores[i][j];
+            //        }
+
+            //        double average = sum / scores[i].Length;
+
+            //        Console.WriteLine($"student {i + 1}'s score is: {average:F2}");
+            //    }
+
+
+            //Random rand = new Random();
+
+            //int[] passcodes = new int[10];
+
+
+            //for (int i = 0; i < passcodes.Length; i++)
+            //{
+            //    passcodes[i] = rand.Next(1000,1001);
+            //}
+
+            //Console.Write("enter 4 digit code: ");
+            //int code = int.Parse(Console.ReadLine());
+
+            //bool found = false;
+
+
+            //for (int i = 0; i < passcodes.Length; i++)
+            //{
+            //    if (passcodes[i] == code)
+            //    {
+            //        found = true;
+            //        break;
+            //    }
+            //}
+
+            //if (found)
+            //    Console.WriteLine("Correct");
+            //else
+            //    Console.WriteLine("Wrong");
+
+
+
+            //int[] numbers = { 15, -7, 23, 0, -12, 8, 31, -3 };
+
+            //int min = numbers[0];
+            //int max = numbers[0];
+
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] < min)
+            //    {
+            //        min = numbers[i];
+            //    }
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+            //}
+
+            //Console.WriteLine("minimum number: " + min);
+            //Console.WriteLine("maximum number: " + max);
+
+
+        }
+    }
+}
---
+++ davaleba-6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace davaleba_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] scores =
+        {
+            new int[] { 90, 85, 88 },      
+            new int[] { 75, 80 },          
+            new int[] { 100, 95, 98, 92 }  
+        };
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < scores[i].Length; j++)
+                {
+                    sum += scores[i][j];
+                }
+
+                double average = sum / scores[i].Length;
+
+                Console.WriteLine($"სტუდენტი {i + 1}-ის საშუალო ქულაა: {average:F2}");
+            }
+        }
+    }
+}
