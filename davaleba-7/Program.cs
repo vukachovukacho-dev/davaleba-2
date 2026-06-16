6feaf24198decefd58b8995e37cafcb7d0a90504
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-16 21:38:55 +0400

    davaleba-4

---
+++ davaleba-7/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace davaleba_7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ davalebaa-4/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace davalebaa_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter number:");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{number} * {i} = {number * i}");
+            //}
+
+            //Console.Write("Enter number:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+
+            //    Console.WriteLine();
+
+            //}
+
+
+            //Console.Write("Enter number:");
+            //int number = int.Parse(Console.ReadLine());
+
+            //int sum = 0;
+
+            //for (int i = 2; i <= number; i += 2)
+            //{
+            //    sum += i;
+            //}
+
+            //Console.WriteLine("Even numbers sum: " + sum);
+
+
+            //Random random = new Random();
+            //int secretNumber = random.Next(1, 101); // 1-დან 100-მდე
+
+            //int guess = 0;
+
+            //while (guess != secretNumber)
+            //{
+            //    Console.Write("Enter number:");
+            //    guess = int.Parse(Console.ReadLine());
+
+            //    if (guess < secretNumber)
+            //    {
+            //        Console.WriteLine("Try a bigger number!");
+            //    }
+            //    else if (guess > secretNumber)
+            //    {
+            //        Console.WriteLine("Try a smaller number!");
+            //    }
+            //}
+
+            //Console.WriteLine("nice you guessed right: " + secretNumber);
+
+
+
+        }
+    }
+}
