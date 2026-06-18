57420e15b331d9620c8b4f13564e05426af89660
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-18 19:36:25 +0400

    davaleba-3

---
+++ davaleba-3/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace davaleba_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string correctUsername = "admin";
+            //string correctPassword = "1234";
+
+            //Console.Write("Enter username: ");
+            //string username = Console.ReadLine();
+
+            //Console.Write("Enter password: ");
+            //string password = Console.ReadLine();
+
+            //if (username == correctUsername && password == correctPassword)
+            //{
+            //    Console.WriteLine("Welcome!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Access denied");
+            //}
+
+
+            //Console.Write("Enter first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter operator (+, -, *, /): ");
+            //string op = Console.ReadLine();
+
+            //Console.Write("Enter second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //double result;
+
+            //switch (op)
+            //{
+            //    case "+":
+            //        result = num1 + num2;
+            //        Console.WriteLine("Result: " + result);
+            //        break;
+
+            //    case "-":
+            //        result = num1 - num2;
+            //        Console.WriteLine("Result: " + result);
+            //        break;
+
+            //    case "*":
+            //        result = num1 * num2;
+            //        Console.WriteLine("Result: " + result);
+            //        break;
+
+            //    case "/":
+            //        if (num2 != 0)
+            //        {
+            //            result = num1 / num2;
+            //            Console.WriteLine("Result: " + result);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Cannot divide by zero!");
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid operator!");
+            //        break;
+            //}
+
+            //Console.Write("Enter your age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //if (age >= 0 && age <= 12)
+            //{
+            //    Console.WriteLine("Child");
+            //}
+            //else if (age >= 13 && age <= 19)
+            //{
+            //    Console.WriteLine("Teenager");
+            //}
+            //else if (age >= 20 && age <= 64)
+            //{
+            //    Console.WriteLine("Adult");
+            //}
+            //else if (age >= 65)
+            //{
+            //    Console.WriteLine("Pensioner");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid age");
+
+            //}
+        }
+    }
+}
