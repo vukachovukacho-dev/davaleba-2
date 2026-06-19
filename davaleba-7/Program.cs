ddf753119bedfe8ef34e72163070db14620f571d
vukachovukacho-dev <vukachovukacho@gmail.com>
2026-06-19 21:39:53 +0400

    davaleba-7

--- davaleba-7/Program.cs
+++ davaleba-7/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace davaleba_7
 {
@@ -6,7 +7,61 @@ namespace davaleba_7
     {
         static void Main()
         {
+            //Console.WriteLine(Pow(2, 3));
+            //int Pow(int num1,int num2)
+            //{
+            //    int result = num1;
+            //    for (int i = 1; i < num2; i++)
+            //    {
+            //        result *= num1;
+            //    }
+            //    return result;
+            //}
+
+
+            //    int num = 30;
+
+            //    if (IsPrime(num))
+            //    {
+            //        Console.WriteLine($"{num}is a simple number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{num} is not a simple number");
+            //    }
+            //}
+
+            //static bool IsPrime(int num)
+            //{
+            //    if (num <= 1) return false;
+
+            //    for (int i = 2; i < num; i++)
+            //    {
+            //        if (num % i == 0)
+            //        {
+            //            return false;
+            //        }
+            //    }
+
+            //    return true;
+
+        //    Console.Write("Enter number: ");
+        //    int num = int.Parse(Console.ReadLine());
+
+        //    PrintBinary(num);
+        //}
+
+        //static void PrintBinary(int n)
+        //{
             
+        //    if (n == 0)
+        //        return;
+
+            
+        //    PrintBinary(n / 2);
+        //    Console.Write(n % 2);
+
         }
     }
 }
+
